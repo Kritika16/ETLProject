51a3b4613458e09bb02ce0dc39931eabd082444f
Jay <Jay@KriJay>
2017-03-05 15:35:51 +0530

    Signed-off-by: Jay <Jay@KriJay>

--- src/test/java/applicationPages/LogInPage.java
+++ src/test/java/applicationPages/LogInPage.java
@@ -3,6 +3,9 @@ package applicationPages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import dataProvider.ExcelDataProvider;
 
@@ -25,6 +28,8 @@ public class LogInPage {
 	@FindBy(xpath = "//input[@class = 'en btn color2 large pull-right input_submit']")
 	WebElement loginButton;
 
+	@FindBy(xpath="//a[text()=' Dashboard ']//following::div[2]")
+	WebElement dashboardtitle;
 	
 	//input[@class = 'en btn color2 large pull-right input_submit']
 	
@@ -42,5 +47,15 @@ public class LogInPage {
 	
 	public void clickLogin(){
 		loginButton.click();
+	}
+
+	public void verifyDashboardTitle(){
+		WebDriverWait wait = new WebDriverWait(driver,20);
+		
+		WebElement ele = wait.until(ExpectedConditions.visibilityOf(dashboardtitle));
+		
+		String text = ele.getText();
+		
+		Assert.assertEquals(text, "Account Dashboard View","Dashboard title not verified properly.");
 	}
 }
